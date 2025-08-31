615ff5577a15d82afc3231660eda95ea1cfbb625
ZEROST <xrootone@gmail.com>
2025-08-31 12:27:44 +0900

    현재 구조까지 잡은 상태

--- apps/admin/nuxt.config.ts
+++ apps/admin/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  layers: ['../../packages/ui-lib'],
+  extends: ['../../packages/ui-lib'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
--- packages/ui-lib/nuxt.config.ts
+++ packages/ui-lib/nuxt.config.ts
@@ -1,3 +1,7 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,5 +13,6 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  css: [ resolve(__dirname, './app/assets/css/main.css') ],
 })
